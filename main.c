30e567831289c8483029ebd439743b3b2df8fbba
sir-isaac-newtools <d102280@embitel.de>
2023-03-03 17:14:49 +0100

    fixing test

--- main.c
+++ main.c
@@ -8,34 +8,10 @@
 int main (int argc, char * argv[])
 {
 
-    if(argc < 2)
-    {
-        printf("invalid parameters\n");
-        exit(0);
-    }
-
-    int option = atoi(argv[1]);
-
-    switch (option)
-    
-    {
-
-    case 0:
-
-     //   TEST_01();
-      //  TEST_02();
-     //   TEST_03();
-        TEST_04();
-
-        break;
-    
-    default:
-        printf("invalid option !!\n");
-        break;
-    }
-    
 
 
+      TEST_01();
+      TEST_02();
+      TEST_03();
 
-    return 0;
 }
--- test.h
+++ test.h
@@ -4,6 +4,5 @@
 void    TEST_01(void);
 void    TEST_02(void);
 void    TEST_03(void);
-void    TEST_04();
 
 #endif
